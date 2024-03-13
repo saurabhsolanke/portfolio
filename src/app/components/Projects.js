edb4e1fafb82861ee225be3364bb61bf8404e513
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2024-03-13 09:09:36 +0530

    SS Svg's

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,17 +1,22 @@
 export default function Projects() {
     return (
         <div>
-            <ul>
-                <li>
-                    Angular
-                </li>
-                <li>
-                    typescript
-                </li>
-                <li>
-                    javascript
-                </li>
-            </ul>
+            <div class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+                <h2 class="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+                    <svg viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class="h-6 w-6 flex-none">
+                        hi
+                    </svg>
+                    <span class="ml-3">Work</span>
+                </h2>
+                <ol class="mt-6 space-y-4 flex flex-col lg:flex-row lg:space-y-0 lg:space-x-4">
+                    hi    </ol>
+                <a class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full" href="#">
+                    Download CV
+                    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50">
+                        hi
+                    </svg>
+                </a>
+            </div>
         </div>
     )
 }
